e7fedf9a97d18950d46e3a5c1f63a667ef0f6ee3
Createitv <xfy150150@163.com>
2021-11-14 11:36:06 +0800

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by DifferThink on 11/14/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
